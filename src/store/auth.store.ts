3bf2911d6535b157193909ae411ca55c320fa026
Hieu To <tothieu160304@gmail.com>
2025-10-08 11:29:19 +0700

    Upda 2

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
 
 interface AuthState {
   isLoggedIn: boolean;
